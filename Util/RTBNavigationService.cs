2c794c3804f449c1eb16732b09c543a56f1d1c7e
unknown <NicholasYang@EASHDPNOTE032.aaa.ad.ea.com>
2012-10-21 22:12:48 +0800

    mrguokr - 1.2 and 1.3 longlistselector + item template
    things imported:
    1. weibo item template from weibo
    2. datacontextpropagationgrid
    3. richtextboxbuilder
    4. RTBNavigationService
    5. WeiboLinkParser

---
+++ Util/RTBNavigationService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace SanzaiWeibo.Utils
+{
+    public static class RTBNavigationService
+    {
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.RegisterAttached(
+            "Content",
+            typeof(string),
+            typeof(RTBNavigationService),
+            new PropertyMetadata(null, OnContentChanged)
+        );
+
+        public static string GetContent(DependencyObject d)
+        { return d.GetValue(ContentProperty) as string; }
+
+        public static void SetContent(DependencyObject d, string value)
+        { d.SetValue(ContentProperty, value); }
+
+
+        private static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RichTextBox richTextBox = d as RichTextBox;
+            if (richTextBox == null)
+                return;
+
+
+            string content = (string)e.NewValue;
+            if (string.IsNullOrEmpty(content))
+                return;
+
+            richTextBox.Blocks.Clear();
+
+            var cvtr = new WeiboLinkParser();
+
+
+            Paragraph pg = new Paragraph();
+            pg.Inlines.Add(content);
+
+            richTextBox.Blocks.Add(pg);
+
+            d.Dispatcher.BeginInvoke(() =>
+            {
+                richTextBox.Blocks.Clear();
+                richTextBox.Blocks.Add(cvtr.Convert(content, typeof(Paragraph), null, null) as Paragraph);
+
+            });
+
+
+        }
+
+    }
+}
---
+++ Util/RichTextBoxBuilder.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Tasks;
+using System.Windows.Navigation;
+
+namespace SanzaiWeibo.Utils
+{
+        class RichTextBuilder
+        {
+            Paragraph _pgh;
+            public Brush HyperlinkForeground { get; set; }
+            
+            public RichTextBuilder()
+            {
+                _pgh = new Paragraph();
+            }
+            public RichTextBuilder(Paragraph p)
+            {
+                _pgh = p;
+            }
+            public void Add(string text)
+            {
+                _pgh.Inlines.Add(text);
+            }
+            public void AddHyperlink(string text, Uri uri)
+            {
+                /*
+                // testing code
+                var run = new Run();
+                run.TextDecorations = TextDecorations.Underline;
+                run.Foreground = HyperlinkForeground;
+                run.Text = text;
+                _pgh.Inlines.Add(run);
+                return;
+                 */
+
+                Hyperlink _hp = new Hyperlink();
+
+                if (HyperlinkForeground != null)
+                    _hp.Foreground = HyperlinkForeground;
+                _hp.TextDecorations = null;
+
+                if (text[0] == '@')
+                {
+                    /*
+                    var at = new Run();
+                    at.FontFamily = buxton;
+                    at.Text = "@";
+                    _hp.Inlines.Add(at);
+                     */
+
+                    var str = new Run();
+                    str.Text = text;
+                    _hp.Inlines.Add(str);
+
+                    _hp.NavigateUri = uri;
+                    /*
+                    _hp.Click+=new RoutedEventHandler((ss,ee)=>
+                    {
+                        (PhoneApplicationPageLocator.Page as MainPage).SingleTapWithDelay(uri, new System.Windows.Input.GestureEventArgs() );
+                    });
+                     */
+                }
+                else if(text[0]== '#')
+                {
+                    /*
+                    var sharp = new Run();
+                    sharp.FontFamily = buxton;
+                    sharp.Text = "#";
+                    _hp.Inlines.Add(sharp);
+                     */
+
+                    var str = new Run();
+                    str.Text = text;
+                    str.FontFamily = new FontFamily("Segoe WP");
+                    _hp.Inlines.Add(str);
+
+                    _hp.NavigateUri = uri;
+                    /*
+                    _hp.Click+=new RoutedEventHandler((ss,ee)=>
+                    {
+                        (PhoneApplicationPageLocator.Page as MainPage).SingleTapWithDelay(uri, new System.Windows.Input.GestureEventArgs() );
+                    });
+                     */
+                }
+                else
+                {
+                    _hp.Inlines.Add(text);
+                    _hp.Click += delegate(object sender, RoutedEventArgs e)
+                    {
+                        WebBrowserTask wb = new WebBrowserTask();
+                        wb.Uri = uri;
+                        try
+                        {
+                            wb.Show();
+                        }
+                        catch
+                        {
+                            // TODO: should check the navigation status. need reference to the application
+                        }
+                    };
+                }
+
+                _pgh.Inlines.Add(_hp);
+            }
+            private RichTextBox rtb;
+
+            public RichTextBox RichTextBox
+            {
+                get
+                {
+                    var rtb = new RichTextBox();
+                    rtb.Blocks.Add(_pgh);
+                    return rtb;
+                }
+                set { rtb = value; }
+            }
+
+            public RichTextBox GetRichTextBox()
+            {
+                var rtb = new RichTextBox();
+                rtb.Blocks.Add(_pgh);
+                return rtb;
+            }
+
+            public Paragraph Paragraph
+            {
+                get { return _pgh; }
+                set { _pgh = value; }
+            }
+
+        }
+}
