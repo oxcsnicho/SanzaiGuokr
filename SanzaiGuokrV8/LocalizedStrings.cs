281843c4af2d285abf08d784b5fe18e7963fa475
oxcsnicho <oxcsnicho@gmail.com>
2013-05-18 20:17:00 +0800

    created the v8 project

---
+++ SanzaiGuokrV8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SanzaiGuokrV8.Resources;
+
+namespace SanzaiGuokrV8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
