1dcb059fda511127a30a7c6d1f954091d185a1ac
yang <yang@sheepws.local>
2013-05-26 14:02:58 -0700

    v8: View article not working, webbrowser issue
    it is because HTML tag issue. <DOCTYPE tags are not accepted in navigate string.
    an alternative is to save the string to Isolated storate and navigate to that
    file instead

--- SanzaiGuokr/MyWebBrowser.xaml.cs
+++ SanzaiGuokr/MyWebBrowser.xaml.cs
@@ -115,177 +115,6 @@ namespace webbrowsertest
             current.MassageAndShowHTML(value);
         }
         #endregion
-#if false
-        #region SourceUri
-        public static readonly DependencyProperty SourceUriProperty = 
-            DependencyProperty.Register(
-            "SourceUri", typeof(Uri), typeof(MyWebBrowser), 
-            new PropertyMetadata(default(Uri),new PropertyChangedCallback(SourceUriChanged))
-            );
-
-        public Uri SourceUri
-        {
-            get
-            {
-                return (Uri)GetValue(SourceUriProperty);
-            }
-            set
-            {
-                SetValue(SourceUriProperty, value);
-            }
-        }
-
-        static void SourceUriChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var current = sender as MyWebBrowser;
-            var value = e.NewValue as Uri;
-            if (current == null || value == null)
-                return;
-
-            current.StartNavigate();
-        }
-
-                private void PrepareControl()
-        {
-            // fix bug #1 in a brutal way
-            var d = DataContext as ReadArticleViewModel;
-            if (d != null)
-            {
-                d.LoadingIndicator = true;
-            }
-
-            if (StartNavigating != null)
-            {
-                StartNavigating(this, new NavigatingEventArgs());
-            }
-            Opacity = 0;
-            LoadCompleted += new LoadCompletedEventHandler((ss, ee) =>
-                {
-                    Opacity = 1;
-                });
-        }
-
-        private void StartNavigate()
-        {
-            PrepareControl();
-            LoadDataAndShowHTML();
-        }
-
-        private void LoadDataAndShowHTML()
-        {
-            Common.RestSharpLoadDataFromUri(SourceUri, (response) =>
-            {
-                Deployment.Current.Dispatcher.BeginInvoke(() =>
-                {
-                    if (response.ResponseStatus != ResponseStatus.Completed
-                        || response.Content.Length == 0)
-                    {
-                        if (NavigationFailed != null)
-                            NavigationFailed(this, null);
-
-                        Messenger.Default.Send<MyWebBrowserStatusChanged>(new MyWebBrowserStatusChanged() { NewStatus = "Rendering" });
-                        InternalWB.NavigateToString(Common.ErrorHtml);
-                    }
-                    else
-                        Deployment.Current.Dispatcher.BeginInvoke(() => MassageAndShowHTML(WebForegroundColor,
-                            WebBackgroundColor, FontSize, response.Content));
-                });
-            });
-        }
-
-        public void MassageAndShowHTML(string html_doc)
-        {
-            MassageAndShowHTML(WebForegroundColor, WebBackgroundColor, WebFontSize, html_doc);
-        }
-        public void MassageAndShowHTML(Color WebForegroundColor, Color WebBackgroundColor, double WebFontSize, string html_doc) // can be changed to async method
-        {
-            var bw = new BackgroundWorker();
-            var mode = HtmlMode; // must needed because going down we are not in the UI thread anymore
-            bw.DoWork += new DoWorkEventHandler((ss, ee) =>
-            {
-                string foreground = WebForegroundColor.ToString().Substring(3).ToLowerInvariant();
-                string base_url = "http://www.guokr.com";
-                string stylesheet = string.Format("<style type=\"text/css\"> "
-                       + "body {{ background-color: #{0};font-size: {2}px }}" //body styles
-                       + "p.document-figcaption{{ font-size: {3}px;font-style:italic;text-align:center}}" // img caption styles
-                        + ".article>article,.article > article h1, .article > article h2, .article > article h3 {{color:#{4} }}" //foreground color 1
-                        + "a, .fake_a {{color:#{5}}}"//foreground color 2
-                        + ".article > article > .title {{padding-top:0px}}" //title gap
-                       + "</style>",
-                    WebBackgroundColor.ToString().Substring(3), foreground, FontSizeTweak(WebFontSize).ToString(), //body style parameters
-                    (FontSizeTweak(WebFontSize) - 1).ToString(), //img caption style parameters
-                    foreground, foreground // foreground color
-                    );
-
-                switch (mode)
-                {
-                    case HtmlModeType.HtmlFragment:
-                        html_doc = @"<!DOCTYPE HTML>
-<html lang=""en"">
-<head>
-    <meta charset=""UTF-8"">
-    <title>http://www.guokr.com/?reply_count=55</title>
-    <link rel=""stylesheet"" href=""http://www.guokr.com/skin/mobile_app.css?gt"">
-    <meta name=""viewport"" content = ""width = device-width, initial-scale = 1, minimum-scale = 1, maximum-scale = 1"" />
-" + stylesheet + @"<body><div class=""cmts"" id=""comments"">" + html_doc + "</div></body></html>";
-                        break;
-
-                    case HtmlModeType.FullHtml:
-                        var index_of_stylesheet = html_doc.IndexOf("/skin/mobile_app.css", StringComparison.InvariantCultureIgnoreCase);
-                        var index_of_head_ending = html_doc.IndexOf("</head>", StringComparison.InvariantCultureIgnoreCase);
-
-                        html_doc = html_doc.Substring(0, index_of_stylesheet)
-                            + base_url
-                            + html_doc.Substring(index_of_stylesheet, index_of_head_ending - index_of_stylesheet)
-                            + stylesheet
-                            + html_doc.Substring(index_of_head_ending, html_doc.Length - index_of_head_ending);
-                        break;
-                }
-
-                html_doc = ConvertExtendedASCII(html_doc);
-
-                Deployment.Current.Dispatcher.BeginInvoke(() =>
-                    {
-                        try
-                        {
-                            Messenger.Default.Send<MyWebBrowserStatusChanged>(new MyWebBrowserStatusChanged() { NewStatus = "Rendering" });
-                            InternalWB.NavigateToString(html_doc);
-                        }
-                        catch
-                        {
-                        }
-                    });
-
-            });
-            bw.RunWorkerAsync();
-        }
-        public static double FontSizeTweak(double a)
-        {
-            return Math.Ceiling((a + 1.328) / 1.333) - 3;
-        }
-        public static string ConvertExtendedASCII(string HTML)
-        {
-            string retVal = "";
-            char[] s = HTML.ToCharArray();
-
-            foreach (char c in s)
-            {
-                if (Convert.ToInt32(c) > 127)
-                    retVal += "&#" + Convert.ToInt32(c) + ";";
-                else
-                    retVal += c;
-            }
-
-            return retVal;
-        }   
-        private void StartNavigate()
-        {
-            PrepareControl();
-            LoadDataAndShowHTML();
-        }
-
-        #endregion
-#endif
 
         #region LoadHTML
 
@@ -356,26 +185,26 @@ b[i].onclick=function () { window.external.notify(thumbnailToImage(this.src)); }
 b[i].onerror=function () { this.src = thumbnailToImage(this.src);};
 }
 function thumbnailToImage(c){
-	var a=c;
-	-1!=a.indexOf(""/thumbnail/"") && (a = a.replace(""thumbnail"",""image"").replace(/\_[0-9]*x\./,"".""));
-	return a;
+    var a=c;
+    -1!=a.indexOf(""/thumbnail/"") && (a = a.replace(""thumbnail"",""image"").replace(/\_[0-9]*x\./,"".""));
+    return a;
 }
 function imageToThumbnail(c){
-	var a=c;
-	if(-1!==a.indexOf(""/thumbnail/""))
-	{
-		a=a.replace(/\_[0-9]*x\./, ""_200x."");
-	}
-	else if(-1!==a.indexOf(""/image/""))
-	{
+    var a=c;
+    if(-1!==a.indexOf(""/thumbnail/""))
+    {
+        a=a.replace(/\_[0-9]*x\./, ""_200x."");
+    }
+    else if(-1!==a.indexOf(""/image/""))
+    {
 -1!==a.indexOf(""image"") && (a=a.replace(""image"", ""thumbnail""));
 -1!==a.indexOf("".jpg"") && (a=a.replace("".jpg"", ""_200x.jpg""));
 -1!==a.indexOf("".png"") && (a=a.replace("".png"", ""_200x.png""));
 -1!==a.indexOf("".gif"") && (a=a.replace("".gif"", ""_200x.gif""));
 }
-	return a;
+    return a;
 }
-					</script>
+                    </script>
 ";
                 string copyright = @"
 <p class=""copyright"">
@@ -430,11 +259,11 @@ function imageToThumbnail(c){
                         html_doc =
                             @"
 <html>
-	<head>
-		<meta charset=""UTF-8"">
-		<link rel=""stylesheet"" href=""http://www.guokr.com/skin/group.css?ss9"">
+    <head>
+        <meta charset=""UTF-8"">
+        <link rel=""stylesheet"" href=""http://www.guokr.com/skin/group.css?ss9"">
     <link rel=""stylesheet"" href=""http://www.guokr.com/skin/mobile_app.css?gt"">
-		<style type=""text/css"">a.bshareDiv,#bsPanel,#bsMorePanel,#bshareF{border:none;background:none;padding:0;margin:0;font:12px Helvetica,Calibri,Tahoma,Arial,宋体,sans-serif;line-height:14px;}#bsPanel div,#bsMorePanel div,#bshareF div{display:block;}.bsRlogo .bsPopupAwd,.bsRlogoSel .bsPopupAwd,.bsLogo .bsPopupAwd,.bsLogoSel .bsPopupAwd{ line-height:16px!important;}a.bshareDiv div,#bsFloatTab div{*display:inline;zoom:1;display:inline-block;}a.bshareDiv img,a.bshareDiv div,a.bshareDiv span,a.bshareDiv a,#bshareF table,#bshareF tr,#bshareF td{text-decoration:none;background:none;margin:0;padding:0;border:none;line-height:1.2}a.bshareDiv span{display:inline;float:none;}div.buzzButton{cursor:pointer;font-weight:bold;}.buzzButton .shareCount a{color:#333}.bsStyle1 .shareCount a{color:#fff}span.bshareText{white-space:nowrap;}span.bshareText:hover{text-decoration:underline;}a.bshareDiv .bsPromo,div.bshare-custom .bsPromo{display:none;position:absolute;z-index:100;}a.bshareDiv .bsPromo.bsPromo1,div.bshare-custom .bsPromo.bsPromo1{width:51px;height:18px;top:-18px;left:0;line-height:16px;font-size:12px !important;font-weight:normal !important;color:#fff;text-align:center;background:url(http://static.bshare.cn/frame/images/bshare_box_sprite2.gif) no-repeat 0 -606px;}div.bshare-custom .bsPromo.bsPromo2{background:url(http://static.bshare.cn/frame/images/bshare_promo_sprite.gif) no-repeat;cursor:pointer;}</style>
+        <style type=""text/css"">a.bshareDiv,#bsPanel,#bsMorePanel,#bshareF{border:none;background:none;padding:0;margin:0;font:12px Helvetica,Calibri,Tahoma,Arial,宋体,sans-serif;line-height:14px;}#bsPanel div,#bsMorePanel div,#bshareF div{display:block;}.bsRlogo .bsPopupAwd,.bsRlogoSel .bsPopupAwd,.bsLogo .bsPopupAwd,.bsLogoSel .bsPopupAwd{ line-height:16px!important;}a.bshareDiv div,#bsFloatTab div{*display:inline;zoom:1;display:inline-block;}a.bshareDiv img,a.bshareDiv div,a.bshareDiv span,a.bshareDiv a,#bshareF table,#bshareF tr,#bshareF td{text-decoration:none;background:none;margin:0;padding:0;border:none;line-height:1.2}a.bshareDiv span{display:inline;float:none;}div.buzzButton{cursor:pointer;font-weight:bold;}.buzzButton .shareCount a{color:#333}.bsStyle1 .shareCount a{color:#fff}span.bshareText{white-space:nowrap;}span.bshareText:hover{text-decoration:underline;}a.bshareDiv .bsPromo,div.bshare-custom .bsPromo{display:none;position:absolute;z-index:100;}a.bshareDiv .bsPromo.bsPromo1,div.bshare-custom .bsPromo.bsPromo1{width:51px;height:18px;top:-18px;left:0;line-height:16px;font-size:12px !important;font-weight:normal !important;color:#fff;text-align:center;background:url(http://static.bshare.cn/frame/images/bshare_box_sprite2.gif) no-repeat 0 -606px;}div.bshare-custom .bsPromo.bsPromo2{background:url(http://static.bshare.cn/frame/images/bshare_promo_sprite.gif) no-repeat;cursor:pointer;}</style>
 " + stylesheet + @"</head><body>" + html_doc + "</body></html>";
                         break;
                 }
@@ -442,7 +271,11 @@ function imageToThumbnail(c){
 #if DEBUG
                 Messenger.Default.Send<MyWebBrowserStatusChanged>(new MyWebBrowserStatusChanged() { NewStatus = "Converting" });
 #endif
+#if WINDOWS_PHONE
                 html_doc = ConvertExtendedASCII(html_doc);
+#else
+                html_doc = html_doc.Substring(html_doc.IndexOf("<html"));
+#endif
 
                 Deployment.Current.Dispatcher.BeginInvoke(() =>
                 {
