2e3590ec46125932e0618ba6978de8f2761a365b
yang <yang@sheepws.local>
2014-09-16 09:46:37 -0700

    update message

--- SanzaiGuokrV8/ChannelsUserControl.xaml.cs
+++ SanzaiGuokrV8/ChannelsUserControl.xaml.cs
@@ -18,10 +18,10 @@ namespace SanzaiGuokr
             InitializeComponent();
 
             text_to_show.Text = "==此处暂时征用用于发布公告==\n\n";
-            text_to_show.Text += "最近因为果壳改版，主题站被砍变成科学人，山寨果壳因为实在不想跟着改版，老的内容又没有了，只好先晾着，请大家耐心等待（再再）下一个更新哦！\n\n";
+            text_to_show.Text += "最近因为懒，还没弄科学人，请大家耐心等待（再再）下一个更新哦！\n\n";
             text_to_show.Text += "==最新改动==\n\n";
             text_to_show.Text += "× 小组翻页\n";
-            text_to_show.Text += "× 透明大图标(没事会翻跟斗那种)\n";
+            text_to_show.Text += "× 透明大图标(没事会翻跟斗————嘚~一下那种)\n";
 
         }
     }
